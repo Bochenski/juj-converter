e0469b45975af90a7205ef8d821c65c11cfcb3a9
David Bochenski <david@bochenski.co.uk>
2012-11-12 12:09:22 +0000

    completed the sale schema

--- convert.js
+++ convert.js
@@ -5,7 +5,16 @@ var db = mongoose.createConnection('localhost', 'juj');
 
 var conn_str = "Driver={SQL Server Native Client 11.0};Server=(local);Database=JUJ;Trusted_Connection={Yes}";
 
-var sale_schema = mongoose.Schema({transaction_id: 'string', sale_price: 'Number', date_of_sale: 'Date'});
+var sale_schema = mongoose.Schema({transaction_id: 'String', 
+                                    sale_price: 'Number', 
+                                    date_of_sale: 'Date',
+                                    stock_code:'String',
+                                    quantity:'Number',
+                                    shop_code:'String',
+                                    transaction_type:'String',
+                                    cost_price:'Number',
+                                    vat_rate:'Number',
+                                    week_num:'Number'});
 
 var Sale = db.model('Sale', sale_schema);
 
@@ -15,21 +24,28 @@ sql.open(conn_str, function (err, conn) {
         console.log("Error opening the connection!");
         return;
     }
-    conn.queryRaw("SELECT TOP 10 transaction_id, sale_price, date_of_sale FROM Sales", function (err, results) {
+    conn.queryRaw("SELECT transaction_id, sale_price, date_of_sale, stock_code, quantity, shop_code, transaction_type, cost_price, vat_rate, week_num FROM Sales", function (err, results) {
         if (err) {
             console.log("Error running query!");
             return;
         }
         for (var i = 0; i < results.rows.length; i++) {
-            console.log("transaction_id: " + results.rows[i][0] + " sale_price: " + results.rows[i][1]);
             var sale = new Sale({transaction_id: results.rows[i][0], 
-                                 sale_price: results.rows[i][1],
-                                 date_of_sale: results.rows[i][2]});
+                                sale_price: results.rows[i][1],
+                                date_of_sale: results.rows[i][2]},
+                                stock_code: results.rows[i][3],
+                                quantity: results.rows[i][4],
+                                shop_code: results.rows[i][5],
+                                transaction_type: results.rows[i][6],
+                                cost_price: results.rows[i][7],
+                                vat_rate: results.rows[i][8],
+                                week_num: results.rows[i][9]);
             sale.save(function (err) {
                 if (err) {
                     console.log('there was an error');
                 }
             })
         }
+        console.log('all done');
     });
 });
