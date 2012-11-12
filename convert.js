e3b5ae719fcfb1f77b81467c96db7005db7c8699
David Bochenski <david@bochenski.co.uk>
2012-11-12 12:01:17 +0000

    trying a date type

--- convert.js
+++ convert.js
@@ -5,7 +5,7 @@ var db = mongoose.createConnection('localhost', 'juj');
 
 var conn_str = "Driver={SQL Server Native Client 11.0};Server=(local);Database=JUJ;Trusted_Connection={Yes}";
 
-var sale_schema = mongoose.Schema({transaction_id: 'string', sale_price: 'Number'});
+var sale_schema = mongoose.Schema({transaction_id: 'string', sale_price: 'Number', date_of_sale: 'Date'});
 
 var Sale = db.model('Sale', sale_schema);
 
@@ -15,14 +15,16 @@ sql.open(conn_str, function (err, conn) {
         console.log("Error opening the connection!");
         return;
     }
-    conn.queryRaw("SELECT TOP 10 transaction_id, sale_price FROM Sales", function (err, results) {
+    conn.queryRaw("SELECT TOP 10 transaction_id, sale_price, date_of_sale FROM Sales", function (err, results) {
         if (err) {
             console.log("Error running query!");
             return;
         }
         for (var i = 0; i < results.rows.length; i++) {
             console.log("transaction_id: " + results.rows[i][0] + " sale_price: " + results.rows[i][1]);
-            var sale = new Sale({transaction_id: results.rows[i][0], sale_price: results.rows[i][1]});
+            var sale = new Sale({transaction_id: results.rows[i][0], 
+                                 sale_price: results.rows[i][1],
+                                 date_of_sale: results.rows[i][2]});
             sale.save(function (err) {
                 if (err) {
                     console.log('there was an error');
